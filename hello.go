4eaeb82857427466a5ac32180c89d08539207a16
Howard Passmore <HowardPassmore@live.com>
2015-07-16 14:44:06 -0700

    I am the king of 7's

--- hello.go
+++ hello.go
@@ -6,6 +6,10 @@ func main() {
 	for i :=0; i<100; i++ {
 		fmt.Println("lol")
 	}
+
 	fmt.Println("Fuq da poliec")
+
+	fmt.Println("I beat chris at ping pong. And being a good person.")
+
 	fmt.Printf("Brady Sullivan is a bitch.\n")
 }
